6e24453f2c29589e5fb6d9153ed1675ab2fc5740
Ihar Hauryliuk <ihar.hauryliuk@gmail.com>
2021-10-26 17:45:27 +0300

    feat: implement FileStructure component

---
+++ src/components/file-structure/FileStructure.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="file-structure" @file-select="fileSelectHandler">
+    <directory :directory="dirData" :nestingLevel="1"></directory>
+  </div>
+</template>
+
+<script>
+import data from '../../../public/static/node_modules.json';
+import Directory from '../directories/Directory.vue';
+
+export default {
+  name: 'FileStructure',
+  data() {
+    return {
+      dirData: data,
+      selectedFile: null,
+    };
+  },
+  components: {
+    Directory,
+  },
+  provide() {
+    return {
+      fileSelectHandler: this.fileSelectHandler,
+    };
+  },
+  methods: {
+    fileSelectHandler(file) {
+      if (this.selectedFile) {
+        this.selectedFile.isSelected = false;
+      }
+      this.selectedFile = file;
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
